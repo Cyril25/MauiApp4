6106536239674950ad074219c506f5203db8a6fe
Samson Cyril <SamsonC@ne.ch>
2025-12-29 09:29:29 +0100

    Projet webassembly

--- MauiApp4/BlazorApp4/Program.cs
+++ MauiApp4/BlazorApp4/Program.cs
@@ -1,11 +1,13 @@
 using BlazorApp4;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Shared.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<FirebaseService>();
 
 await builder.Build().RunAsync();
